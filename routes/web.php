ffe392e2da56d0c1eca291945140c6390f5577e6
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-10-25 18:41:00 +0900

    web.phpにURLの作成、view表示、

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/comments/{timeZone}', function ($timeZone) {
+    return view('greeting', ['timeZone' => $timeZone]);
 });
